971d9cf37bad3a7c29e9ec129d1f3be5c18f92f8
Bartek <bartekd1998@gmail.com>
2025-09-02 20:45:05 +0200

    feature: add main routine to scrap offers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from google_sheets_client import GoogleSheetClient
 from pracuj_scraper import PracujScraper
 
 import asyncio
@@ -6,14 +7,22 @@ from playwright.async_api import async_playwright
 
 async def main():
     async with async_playwright() as p:
-        browser = await p.chromium.launch()
+        browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
         pracuj_scraper = PracujScraper(page, browser)
         await pracuj_scraper.navigate()
         await pracuj_scraper.accept_cookies()
         await pracuj_scraper.search("test python", None)
-        await pracuj_scraper.extract_job_data()
+        await pracuj_scraper.sort_offers_from_newest()
+        gc = GoogleSheetClient()
+        gc.open_spreadsheet('job-offers')
+        links = gc.get_worksheet(0).col_values(5)
+        await pracuj_scraper.extract_job_data(links)
         jobs = pracuj_scraper.all_jobs
-        print(jobs)
-
+        gc = GoogleSheetClient()
+        gc.open_spreadsheet('Example spreadsheet')
+        columns = ["employer", "position", "earning", "requirements", "url", "status"]
+        rows = [[job.get(col, "") for col in columns] for job in jobs]
+        gc.get_worksheet(0).insert_rows(rows, 2)
 asyncio.run(main())
+
