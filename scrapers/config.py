2d622353b92ae9212189494189fbc39c25c98792
Bartek <bartekd1998@gmail.com>
2025-10-20 20:39:45 +0200

    fix: add self in ScraperCofig

--- scrapers/config.py
+++ scrapers/config.py
@@ -7,20 +7,20 @@ class ScraperConfig:
     """Configuration for all jobs scrapers"""
     def __init__(self):
         # Google Sheets
-        credentials_path: str = os.getenv("GOOGLE_CREDENTIALS_PATH", "credentials.json")
-        spreadsheet_name: str = os.getenv("SPREADSHEET_NAME", "job-offers")
+        self.credentials_path: str = os.getenv("GOOGLE_CREDENTIALS_PATH", "credentials.json")
+        self.spreadsheet_name: str = os.getenv("SPREADSHEET_NAME", "job-offers")
 
         # Search params
-        search_keywords: str = os.getenv("SEARCH_KEYWORDS", 'python test')
-        search_location: str = os.getenv("SEARCH_LOCATION", "Łódź")
+        self.search_keywords: str = os.getenv("SEARCH_KEYWORDS", 'python test')
+        self.search_location: str = os.getenv("SEARCH_LOCATION", "Łódź")
 
         # Scraping
-        max_open_pages: int = int(os.getenv("MAX_OPEN_PAGES", '5'))
-        scroll_step: int = 400
+        self.max_open_pages: int = int(os.getenv("MAX_OPEN_PAGES", '5'))
+        self.scroll_step: int = 400
 
         # Timeouts
-        page_load_timeout: int = 30000
-        element_wait_timeout: int = 5000
+        self.page_load_timeout: int = 30000
+        self.element_wait_timeout: int = 5000
 
     @classmethod
     def from_env(cls):
