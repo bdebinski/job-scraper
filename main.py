8e3848b8f21dce99517a888620b7b629214ddfd3
Bartek <bartekd1998@gmail.com>
2026-01-26 15:49:11 +0100

    experimental: change semaphore to 5

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def run_scraper(scraper_class, urls, config, sem=None):
         context = await browser.new_context(
             **context_args
         )
-        scraper = scraper_class(context, browser, 1)
+        scraper = scraper_class(context, browser, 5)
         print(f"go to {scraper_class}")
         await scraper.navigate()
         print(f"accept cookies {scraper_class}")
